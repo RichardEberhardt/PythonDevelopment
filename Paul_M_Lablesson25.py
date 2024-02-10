37e025be8c9e70d41714d92af22b7cf7d276cdb5
RichardEberhardt <59894605+RichardEberhardt@users.noreply.github.com>
2024-02-10 12:41:36 -0600

    Update Paul_M_Lablesson25.py

--- Paul_M_Lablesson25.py
+++ Paul_M_Lablesson25.py
@@ -10,7 +10,6 @@
 # 	Functionality
 # 		Display the temperature and humidity on the LCD
 # 		toggle button to swap between centigrade or fahrenheit
-# end of comments
 
 import RPi.GPIO as GPIO
 import LCD1602
@@ -26,23 +25,41 @@ GPIO.setup(dht11Pin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
 sensor = adafruit_dht.DHT11(board.D26)
 
+while True:
 
-try:
+	try:
 
-	while True:
 		# Print the values to the serial port
-		temperature_c = sensor.temperature
+		temperature_c 			= sensor.temperature
+
+		temperature_c_disp		="Temp={0:0.1f}C".format(temperature_c)
+		
 		temperature_f = temperature_c * (9 / 5) + 32
+		temperature_f_disp		= "Temp={0:0.1f}F".format(temperature_f)
+
 		humidity = sensor.humidity
+		humidity_disp = "Humidity={0:0.1f}".format(humidity)
+		
 		print("Temp={0:0.1f}ºC, Temp={1:0.1f}ºF, Humidity={2:0.1f}%".format(temperature_c, 		temperature_f, humidity))
 		
-		sleep(2)
+		LCD1602.write(0,0,temperature_f_disp)
+		LCD1602.write(0,1,humidity_disp)
 		
-#		LCD1602.write(0,0,'Hello World')
-#		LCD1602.write(0,1,'Welcome!')
+		time.sleep(2)
 
-except KeyboardInterrupt:
+	except KeyboardInterrupt:	
+		LCD1602.clear()
+		GPIO.cleanup()
+		print("LCD 	Good to Go")
+		exit()		
+			
+	except RuntimeError as error:
+		# Errors happen fairly often, DHT's are hard to read, just keep going
+		print(error.args[0])
+		time.sleep(2.0)
+		continue
+		
+	except Exception as error:
+		dhtDevice.exit()
+		raise error
 
-	LCD1602.clear()
-	print("LCD 	Good to Go")
-	
