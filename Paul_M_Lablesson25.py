e0c8fbffbe4047e5a6a34ef684b9d1b7666d2d57
RichardEberhardt <59894605+RichardEberhardt@users.noreply.github.com>
2024-02-16 15:17:14 -0600

    Update Paul_M_Lablesson25.py

--- Paul_M_Lablesson25.py
+++ Paul_M_Lablesson25.py
@@ -10,6 +10,7 @@
 # 	Functionality
 # 		Display the temperature and humidity on the LCD
 # 		toggle button to swap between centigrade or fahrenheit
+#
 
 import RPi.GPIO as GPIO
 import LCD1602
@@ -19,16 +20,46 @@ import board
 
 LCD1602.init(0x27, 1)
 
+# pin for reading the sensor
 dht11Pin=26
+# pin for the switch
+inButtonPin=20
+
 GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(dht11Pin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(inButtonPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+currentTemperatureType = 'F'
+
+lastInButtonState=-1
+currentInButtonState=-1
+
+ON_OFF=True
 
 sensor = adafruit_dht.DHT11(board.D26)
 
 while True:
 
 	try:
-
+	
+	
+		currentInButtonState=GPIO.input(inButtonPin)
+		
+		print('current button state is… ',currentInButtonState)
+		
+		if currentInButtonState!=lastInButtonState: #button state changed
+			if currentInButtonState==GPIO.LOW: 		#button was just pressed
+				ON_OFF=not ON_OFF
+				if currentTemperatureType == "F":	#adjust temperature type
+					currentTemperatureType="C"
+				else:
+					currentTemperatureType = 'F'
+			else:	
+				if currentInButtonState==GPIO.HIGH:	#button was just released
+					lastInButtonState=currentInButtonState
+		print('Current Temperature Type: ', currentTemperatureType)		
+					
 		# Print the values to the serial port
 		temperature_c 			= sensor.temperature
 
@@ -36,13 +67,19 @@ while True:
 		
 		temperature_f = temperature_c * (9 / 5) + 32
 		temperature_f_disp		= "Temp={0:0.1f}F".format(temperature_f)
+		
+		if currentTemperatureType=='F':
+			selectedTemperatureFormatted="Temp={0:0.1f}F".format(temperature_f)
+	
+		else:
+			selectedTemperatureFormatted="Temp={0:0.1f}C".format(temperature_c)
 
 		humidity = sensor.humidity
 		humidity_disp = "Humidity={0:0.1f}".format(humidity)
 		
 		print("Temp={0:0.1f}ºC, Temp={1:0.1f}ºF, Humidity={2:0.1f}%".format(temperature_c, 		temperature_f, humidity))
 		
-		LCD1602.write(0,0,temperature_f_disp)
+		LCD1602.write(0,0,selectedTemperatureFormatted)
 		LCD1602.write(0,1,humidity_disp)
 		
 		time.sleep(2)
