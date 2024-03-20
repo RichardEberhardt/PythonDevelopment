9d57022ebda1dfbc22b6f15987a2874d72a77e66
RichardEberhardt <59894605+RichardEberhardt@users.noreply.github.com>
2024-03-20 16:30:41 -0500

    Update Paul_W_Lablesson27_Passive_Buzzer.py

--- Paul_W_Lablesson27_Passive_Buzzer.py
+++ Paul_W_Lablesson27_Passive_Buzzer.py
@@ -1,1 +1,23 @@
-
+#Raspberry pi Lesson 27: passive buzzer
+import RPi.GPIO as GPIO
+import time
+buzzPin=17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(buzzPin,GPIO.OUT)
+buzz=GPIO.PWM(buzzPin,400)
+buzz.start(50)
+try:
+	while True:
+	
+		for i in range(150,2000):
+			buzz.ChangeFrequency(i)
+			time.sleep(.0001)
+			
+		for i in range(2000,150,-1):
+			buzz.ChangeFrequency(i)
+			time.sleep(.0001)
+	
+except KeyboardInterrupt:
+	GPIO.cleanup()
+	print('GPIO is good to go')
+	
